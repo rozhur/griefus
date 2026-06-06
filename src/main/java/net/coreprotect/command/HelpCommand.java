8e20091c2fb75cc60eccfc98b5a1f9d2f182faa8
Roman Zhuravlev <rozhur@zhdev.org>
2026-06-06 22:10:12 +0500

    Add permission checks

--- src/main/java/net/coreprotect/command/HelpCommand.java
+++ src/main/java/net/coreprotect/command/HelpCommand.java
@@ -19,7 +19,8 @@ public class HelpCommand {
                 Chat.sendMessage(player, Phrase.build(Phrase.HELP_HEADER));
                 if (helpcommand.equals("help")) {
                     Chat.sendMessage(player, Phrase.build(Phrase.HELP_CO_HELP));
-                } else if (helpcommand.equals("inspect") || helpcommand.equals("inspector") || helpcommand.equals("in")) {
+                } else if ((helpcommand.equals("inspect") || helpcommand.equals("inspector") || helpcommand.equals("in")) &&
+                        player.hasPermission("griefus.inspect")) {
                     Chat.sendMessage(player, Phrase.build(Phrase.HELP_INSPECT_1));
                     Chat.sendMessage(player, Phrase.build(Phrase.HELP_INSPECT_2));
                     Chat.sendMessage(player, Phrase.build(Phrase.HELP_INSPECT_3));
@@ -35,7 +36,8 @@ public class HelpCommand {
                     Chat.sendMessage(player, Phrase.build(Phrase.HELP_PARAMS_ACTION));
                     Chat.sendMessage(player, Phrase.build(Phrase.HELP_PARAMS_INCLUDE));
                     Chat.sendMessage(player, Phrase.build(Phrase.HELP_PARAMS_EXCLUDE));
-                } else if (helpcommand.equals("rollback") || helpcommand.equals("rollbacks") || helpcommand.equals("rb") || helpcommand.equals("ro")) {
+                } else if ((helpcommand.equals("rollback") || helpcommand.equals("rollbacks") || helpcommand.equals("rb") || helpcommand.equals("ro")) &&
+                        player.hasPermission("griefus.rollback")) {
                     Chat.sendMessage(player, Phrase.build(Phrase.HELP_ROLLBACK_HEADER));
                     Chat.sendMessage(player, Phrase.build(Phrase.HELP_ROLLBACK_USER));
                     Chat.sendMessage(player, Phrase.build(Phrase.HELP_ROLLBACK_TIME));
@@ -43,7 +45,8 @@ public class HelpCommand {
                     Chat.sendMessage(player, Phrase.build(Phrase.HELP_ROLLBACK_ACTION));
                     Chat.sendMessage(player, Phrase.build(Phrase.HELP_ROLLBACK_INCLUDE));
                     Chat.sendMessage(player, Phrase.build(Phrase.HELP_ROLLBACK_EXCLUDE));
-                } else if (helpcommand.equals("restore") || helpcommand.equals("restores") || helpcommand.equals("re") || helpcommand.equals("rs")) {
+                } else if ((helpcommand.equals("restore") || helpcommand.equals("restores") || helpcommand.equals("re") || helpcommand.equals("rs")) &&
+                        player.hasPermission("griefus.restore")) {
                     Chat.sendMessage(player, Phrase.build(Phrase.HELP_RESTORE_HEADER));
                     Chat.sendMessage(player, Phrase.build(Phrase.HELP_RESTORE_USER));
                     Chat.sendMessage(player, Phrase.build(Phrase.HELP_RESTORE_TIME));
@@ -51,37 +54,45 @@ public class HelpCommand {
                     Chat.sendMessage(player, Phrase.build(Phrase.HELP_RESTORE_ACTION));
                     Chat.sendMessage(player, Phrase.build(Phrase.HELP_RESTORE_INCLUDE));
                     Chat.sendMessage(player, Phrase.build(Phrase.HELP_RESTORE_EXCLUDE));
-                } else if (helpcommand.equals("lookup") || helpcommand.equals("lookups") || helpcommand.equals("l")) {
+                } else if ((helpcommand.equals("lookup") || helpcommand.equals("lookups") || helpcommand.equals("l")) &&
+                        player.hasPermission("griefus.lookup")) {
                     Chat.sendMessage(player, Phrase.build(Phrase.HELP_LOOKUP_HEADER, Color.DARK_AQUA));
                     Chat.sendMessage(player, Phrase.build(Phrase.HELP_LOOKUP_ALIAS));
                     Chat.sendMessage(player, Phrase.build(Phrase.HELP_LOOKUP_PAGE));
-                } else if (helpcommand.equals("purge") || helpcommand.equals("purges")) {
+                } else if ((helpcommand.equals("purge") || helpcommand.equals("purges")) &&
+                        player.hasPermission("griefus.purge")) {
                     Chat.sendMessage(player, Phrase.build(Phrase.HELP_PURGE_MAIN));
                     Chat.sendMessage(player, Phrase.build(Phrase.HELP_PURGE_EXAMPLE));
-                } else if (helpcommand.equals("reload")) {
+                } else if (helpcommand.equals("reload") && player.hasPermission("griefus.reload")) {
                     Chat.sendMessage(player, Phrase.build(Phrase.HELP_RELOAD));
-                } else if (helpcommand.equals("status")) {
+                } else if (helpcommand.equals("status") && player.hasPermission("griefus.status")) {
                     Chat.sendMessage(player, Phrase.build(Phrase.HELP_STATUS));
-                } else if (helpcommand.equals("teleport")) {
+                } else if (helpcommand.equals("teleport") && player.hasPermission("griefus.teleport")) {
                     Chat.sendMessage(player, Phrase.build(Phrase.HELP_TELEPORT));
-                } else if (helpcommand.equals("u") || helpcommand.equals("user") || helpcommand.equals("users") || helpcommand.equals("uuser") || helpcommand.equals("uusers")) {
+                } else if ((helpcommand.equals("u") || helpcommand.equals("user") || helpcommand.equals("users") || helpcommand.equals("uuser") || helpcommand.equals("uusers")) &&
+                        canLookupRollbackRestorePurge(player)) {
                     Chat.sendMessage(player, Phrase.build(Phrase.HELP_USER_MAIN));
                     Chat.sendMessage(player, Phrase.build(Phrase.HELP_USER_NOTE));
-                } else if (helpcommand.equals("t") || helpcommand.equals("time") || helpcommand.equals("ttime")) {
+                } else if ((helpcommand.equals("t") || helpcommand.equals("time") || helpcommand.equals("ttime")) &&
+                        canLookupRollbackRestorePurge(player)) {
                     Chat.sendMessage(player, Phrase.build(Phrase.HELP_TIME_MAIN));
                     Chat.sendMessage(player, Phrase.build(Phrase.HELP_TIME_NOTE));
-                } else if (helpcommand.equals("r") || helpcommand.equals("radius") || helpcommand.equals("rradius")) {
+                } else if ((helpcommand.equals("r") || helpcommand.equals("radius") || helpcommand.equals("rradius")) &&
+                        canLookupRollbackRestorePurge(player)) {
                     Chat.sendMessage(player, Phrase.build(Phrase.HELP_RADIUS_MAIN));
                     Chat.sendMessage(player, Phrase.build(Phrase.HELP_RADIUS_NOTE));
-                } else if (helpcommand.equals("a") || helpcommand.equals("action") || helpcommand.equals("actions") || helpcommand.equals("aaction")) {
+                } else if ((helpcommand.equals("a") || helpcommand.equals("action") || helpcommand.equals("actions") || helpcommand.equals("aaction")) &&
+                        canLookupRollbackRestorePurge(player)) {
                     Chat.sendMessage(player, Phrase.build(Phrase.HELP_ACTION_MAIN));
                     Chat.sendMessage(player, Phrase.build(Phrase.HELP_ACTION_NOTE));
-                } else if (helpcommand.equals("i") || helpcommand.equals("include") || helpcommand.equals("iinclude") || helpcommand.equals("b") || helpcommand.equals("block") || helpcommand.equals("blocks") || helpcommand.equals("bblock") || helpcommand.equals("bblocks")) {
+                } else if ((helpcommand.equals("i") || helpcommand.equals("include") || helpcommand.equals("iinclude") || helpcommand.equals("b") || helpcommand.equals("block") || helpcommand.equals("blocks") || helpcommand.equals("bblock") || helpcommand.equals("bblocks")) &&
+                        canLookupRollbackRestorePurge(player)) {
                     Chat.sendMessage(player, Phrase.build(Phrase.HELP_INCLUDE_MAIN));
                     Chat.sendMessage(player, Phrase.build(Phrase.HELP_INCLUDE_NOTE));
                     Chat.sendMessage(player, Phrase.build(Phrase.LINK_WIKI_BLOCK));
                     Chat.sendMessage(player, Phrase.build(Phrase.LINK_WIKI_ENTITY));
-                } else if (helpcommand.equals("e") || helpcommand.equals("exclude") || helpcommand.equals("eexclude")) {
+                } else if ((helpcommand.equals("e") || helpcommand.equals("exclude") || helpcommand.equals("eexclude")) &&
+                        canLookupRollbackRestorePurge(player)) {
                     Chat.sendMessage(player, Phrase.build(Phrase.HELP_EXCLUDE_MAIN));
                     Chat.sendMessage(player, Phrase.build(Phrase.HELP_EXCLUDE_NOTE));
                     Chat.sendMessage(player, Phrase.build(Phrase.LINK_WIKI_BLOCK));
@@ -91,16 +102,23 @@ public class HelpCommand {
             } else {
                 Chat.sendMessage(player, Phrase.build(Phrase.HELP_MAIN_HEADER));
                 Chat.sendMessage(player, Phrase.build(Phrase.HELP_MAIN_COMMAND));
-                Chat.sendMessage(player, Phrase.build(Phrase.HELP_MAIN_INSPECT));
-                Chat.sendMessage(player, Phrase.build(Phrase.HELP_MAIN_ROLLBACK));
-                Chat.sendMessage(player, Phrase.build(Phrase.HELP_MAIN_RESTORE));
-                Chat.sendMessage(player, Phrase.build(Phrase.HELP_MAIN_LOOKUP));
-                Chat.sendMessage(player, Phrase.build(Phrase.HELP_MAIN_PURGE));
-                Chat.sendMessage(player, Phrase.build(Phrase.HELP_MAIN_RELOAD));
-                Chat.sendMessage(player, Phrase.build(Phrase.HELP_MAIN_STATUS));
+                if (player.hasPermission("griefus.inspect")) Chat.sendMessage(player, Phrase.build(Phrase.HELP_MAIN_INSPECT));
+                if (player.hasPermission("griefus.rollback")) Chat.sendMessage(player, Phrase.build(Phrase.HELP_MAIN_ROLLBACK));
+                if (player.hasPermission("griefus.restore")) Chat.sendMessage(player, Phrase.build(Phrase.HELP_MAIN_RESTORE));
+                if (player.hasPermission("griefus.lookup")) Chat.sendMessage(player, Phrase.build(Phrase.HELP_MAIN_LOOKUP));
+                if (player.hasPermission("griefus.purge")) Chat.sendMessage(player, Phrase.build(Phrase.HELP_MAIN_PURGE));
+                if (player.hasPermission("griefus.reload")) Chat.sendMessage(player, Phrase.build(Phrase.HELP_MAIN_RELOAD));
+                if (player.hasPermission("griefus.status")) Chat.sendMessage(player, Phrase.build(Phrase.HELP_MAIN_STATUS));
             }
         } else {
             Chat.sendMessage(player, Phrase.build(Phrase.NO_PERMISSION));
         }
+    }
+
+    private static boolean canLookupRollbackRestorePurge(CommandSender player) {
+        return player.hasPermission("griefus.lookup") ||
+                player.hasPermission("griefus.rollback") ||
+                player.hasPermission("griefus.restore") ||
+                player.hasPermission("griefus.purge");
     }
 }
